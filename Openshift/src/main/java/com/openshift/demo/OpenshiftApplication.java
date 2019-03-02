907283507b104acef530c84ffd221cd1263a89d1
adilwaheed51 <48143562+adilwaheed51@users.noreply.github.com>
2019-03-02 13:40:43 -0500

    Initial commit

--- Openshift/src/main/java/com/openshift/demo/OpenshiftApplication.java
+++ Openshift/src/main/java/com/openshift/demo/OpenshiftApplication.java
@@ -1,5 +1,6 @@
 package com.openshift.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,13 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OpenshiftApplication {
 
+	@Value("${my.name}")
+	private String name;
+
 	public static void main(String[] args) {
 		SpringApplication.run(OpenshiftApplication.class, args);
 	}
 
 	@GetMapping("/hello")
 	public String hello() {
-		return "This is from Openshift version 1.2";
+		return "This is from Openshift version 1.2 " + name;
 	}
 
 }
